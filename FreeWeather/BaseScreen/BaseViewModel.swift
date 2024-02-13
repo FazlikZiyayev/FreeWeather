ae1a9355101fd1a86f27d19a8c905716159d3f12
fazlikziyaev <fazliddin2002.15@gmail.com>
2024-02-13 23:56:59 +0500

    creating model for current temp is done it is demo

--- FreeWeather/BaseScreen/BaseViewModel.swift
+++ FreeWeather/BaseScreen/BaseViewModel.swift
@@ -10,6 +10,8 @@ import Foundation
 
 class BaseViewModel
 {
+    var apiManager: APIManagerProtocol = APIManager()
+    
     var currentTemp: Observable<String> = Observable("0")
     
     
@@ -17,5 +19,37 @@ class BaseViewModel
     func getCurrentTemp() -> String?
     {
         return self.currentTemp.value
+    }
+    
+    
+    
+    func fetchCurrentTemp()
+    {
+        let apiKey = NetworkConstants.shared.apiKey
+        let baseUrl = NetworkConstants.shared.serverEndpoint
+        
+        guard let safeUrl = URL(string: baseUrl + "current.json?key=" + apiKey + "&q=Uzbekistan") else { return }
+        
+        self.apiManager.fetchDataViaAlamofire(from: safeUrl, headers: nil)
+        { [weak self] result in
+            
+            guard let self = self else { return }
+            
+            switch result
+            {
+            case .success(let data):
+                do {
+                    let result = try JSONDecoder().decode(WeatherResponse.self, from: data)
+                    print(result)
+                    self.currentTemp.value = "\(Int(result.current.temp_c))"
+                    
+                } catch {
+                    print(error)
+                }
+                
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
     }
 }
