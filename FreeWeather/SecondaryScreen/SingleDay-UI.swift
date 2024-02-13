e7c023e03cae4fe4da67fb91c625009690206182
fazlikziyaev <fazliddin2002.15@gmail.com>
2024-02-14 02:30:23 +0500

    clean code

---
+++ FreeWeather/SecondaryScreen/SingleDay-UI.swift
@@ -0,0 +1,8 @@
+//
+//  SingleDay-UI.swift
+//  FreeWeather
+//
+//  Created by Fazlik Ziyaev on 14/02/24.
+//
+
+import Foundation
---
+++ FreeWeather/SecondaryScreen/SingleDayViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SingleDayViewModel.swift
+//  FreeWeather
+//
+//  Created by Fazlik Ziyaev on 14/02/24.
+//
+
+import Foundation
